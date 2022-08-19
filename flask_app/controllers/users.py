91cb0af9c0db67eea9626803084c5597010c1136
Dashawn <creswellshawn@gmail.com>
2022-08-19 09:00:44 -0700

    Project Update

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,15 +2,27 @@ from ..models.recipe import Recipe
 from flask_app import app, render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
+from pprint import pprint
+
+from marvel import Marvel
+marvel = Marvel(PUBLIC_KEY="0b340cd6701075fffe6b41aeec6947b3" , PRIVATE_KEY= "a72560e27f40a5eec919b7f04feca520215feb16")
 bcrypt = Bcrypt(app)
 
 # ! Home page
 @app.route("/")
 def index():
-    session.clear()
-    user = User.get_all()
-    print(user)
-    return render_template("index.html", user = user)
+    characters = marvel.characters
+    # img = marvel.image
+    my_characters = characters.all(name = "Black Panther")['data']["results"]
+    pprint("-----------------")
+    for char in my_characters:
+        pprint(char['thumbnail'])
+    # images = characters.all(path = "http://i.annihil.us/u/prod/marvel/i/mg/6/60/5261a80a67e7d")['data']["results"]
+    # {'path': 'http://i.annihil.us/u/prod/marvel/i/mg/6/60/5261a80a67e7d', 'extension': 'jpg'}
+    # my_characters = characters.all(nameStartsWith = "Black")['data']["results"]
+    # pprint("-----------------")
+    # pprint(my_characters)
+    return render_template("Landing.html", my_characters=my_characters)
 
 
 # ! Create User
--- server.py
+++ server.py
@@ -1,33 +1,30 @@
 from flask_app import app
 from flask_app.controllers import recipes
 from flask_app.controllers import users
+from flask_app.controllers import heros
+
 from marvel import Marvel
 # from import PUBLIC_KEY, PRIVATE_KEY
 
-marvel = Marvel(PUBLIC_KEY="0b340cd6701075fffe6b41aeec6947b3" , PRIVATE_KEY= "a72560e27f40a5eec919b7f04feca520215feb16")
+# marvel = Marvel(PUBLIC_KEY="0b340cd6701075fffe6b41aeec6947b3" , PRIVATE_KEY= "a72560e27f40a5eec919b7f04feca520215feb16")
 
-characters = marvel.characters
+# characters = marvel.characters
+
+# my_characters = characters.all(nameStartsWith = "Capt")['data']["results"]
+# print(my_characters[0]["name"])
+# for char in my_characters:
+#     print(char['id'], char["name"])
+#     for comic in char["comics"]["items"]:
+#         print(comic['name'])
+#     print("--------------------")
 
 # my_characters = characters.all(name = "Black Panther")['data']["results"]
-
-
 # print(my_characters[0]["name"])
 # print(my_characters[0]["id"])
 # print(my_characters[0]["comics"])
 
-my_characters = characters.all(nameStartsWith = "Capt")['data']["results"]
-print(my_characters[0]["name"])
-
 # for char in my_characters:
 #     print(char['name'])
-    
-for char in my_characters:
-    print(char['id'], char["name"])
-    for comic in char["comics"]["items"]:
-        print(comic['name'])
-    print("--------------------")
-
-
 
 
 if __name__ == "__main__":
